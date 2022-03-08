5e5df6dfc632416aa24ad0be09e535c61e8edb6f
ishibashi-futos <ishibashi.futos@outlook.com>
2022-03-09 02:05:17 +0900

    基本型についてのお試し環境を追加 基本の数値型についてのテストを記載
    - 数値に_で型を指定することもできる
    - `as`でキャストする際に符号拡張・ゼロ拡張、truncationが発生する

---
+++ lib/basic-types/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    assert_eq!(10_i8 as u16, 10_u16); // in range
+    assert_eq!(2525_u16 as i16, 2525_i16); // in range
+
+    assert_eq!(-1_i16 as i32, -1_i32); // sign-extended
+    assert_eq!(65535_u16 as i32, 65535_i32); // zero-extended
+
+    // 変換先の範囲からはみ出す変換は、元の値の2^Nによる余剰となる
+    // Nは変換先のビット数である
+    assert_eq!(1000_i16 as u8, 232_u8);
+    assert_eq!(65535_u32 as i16, -1_i16);
+    assert_eq!(-1_i8 as u8, 255_u8);
+    assert_eq!(255_u8 as i8, -1_i8);
+}
