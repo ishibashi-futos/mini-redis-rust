17a72bb5d74d9978e4b318093338decc548dabff
ishibashi-futos <ishibashi.futos@outlook.com>
2022-03-19 21:09:40 +0900

    rustのドキュメントの書き方について学んだ なんと、コードブロックを書くとテストとして実行可能になるらしい。意味わからんwww

--- lib/crate-and-modules/src/lib.rs
+++ lib/crate-and-modules/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 pub struct Fern {
     pub size: f64,
     pub growth_rate: f64,
@@ -19,4 +21,21 @@ pub fn run_simulation(fern: &mut Fern, days: usize) {
     for _ in 0..days {
         fern.grow();
     }
+}
+
+/// Return true if two ranges overlap.
+/// 2つのRangeに重なる部分があればtrueを返す
+/// ```
+/// assert_eq!(crate_and_modules::overlap(0..7, 3..10), true);
+/// assert_eq!(crate_and_modules::overlap(1..5, 101..105), false);
+/// ```
+///
+/// If either range is empty, they dont' count as overlapping.
+/// どちらかの範囲が空であれば、重なっていないことにする
+/// ```
+/// assert_eq!(crate_and_modules::overlap(0..0, 0..10), false);
+/// ```
+pub fn overlap(r1: Range<usize>, r2: Range<usize>) -> bool {
+    r1.start < r1.end && r2.start < r2.end &&
+        r1.start < r2.end && r2.start < r1.end
 }
