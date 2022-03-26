1a647c6d8b8f237d4936d8a71244645a72cb6138
ishibashi-futos <ishibashi.futos@outlook.com>
2022-03-26 22:24:27 +0900

    impl Traitの実装方法と、演算子をオーバーロードするトレイトの実装を学んだ イテレータのように関連型があるものもある

--- lib/trait-types/src/visible.rs
+++ lib/trait-types/src/visible.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Mul};
+
 pub struct Canvas {}
 
 impl Canvas {
@@ -39,5 +41,57 @@ impl Visible for Broom {
 }
 
 // Creatureを実装する型は、Visibleも実装していなければならない
-pub trait Creature: Visible {
+pub trait Creature: Visible {}
+
+#[derive(Debug, PartialEq)]
+pub struct Point {
+    pub x: u32,
+    pub y: u32,
+}
+
+impl Mul for Point {
+    type Output = Point;
+
+    fn mul(self, rhs: Point) -> Point {
+        Point {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+        }
+    }
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Point) -> Point {
+        Point {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+#[test]
+fn point_mul() {
+    let point1 = Point { x: 10, y: 10 };
+    let point2 = Point { x: 2, y: 3 };
+
+    let actual = point1 * point2;
+
+    assert_eq!(Point { x: 20, y: 30 }, actual);
+}
+
+#[allow(dead_code)]
+fn cyclical_zip(v: Vec<u8>, u: Vec<u8>) -> impl Iterator<Item = u8> {
+    v.into_iter().chain(u.into_iter()).cycle()
+}
+
+#[test]
+#[ignore]
+fn test_cyclical_zip() {
+    let v = vec![10, 20, 30, 40];
+    let u = vec![40, 30, 20, 10];
+
+    // 無限ループになるので注意
+    let _iterator = cyclical_zip(v, u);
 }
