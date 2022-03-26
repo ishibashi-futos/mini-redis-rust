8c80fb43480e5c422ad3933acda61931f3a85887
ishibashi-futos <ishibashi.futos@outlook.com>
2022-03-26 21:09:41 +0900

    dynは動的ディスパッチになるためオーバーヘッドがある。

--- lib/trait-types/src/write.rs
+++ lib/trait-types/src/write.rs
@@ -1,6 +1,6 @@
-use std::io::Write;
+use std::{fmt::Debug, hash::Hash, io::Write};
 
-pub fn say_hello(out: &mut dyn Write) -> std::io::Result<()> {
+pub fn say_hello<W: Write>(out: &mut W) -> std::io::Result<()> {
     out.write_all(b"Hello, World\n")?;
     out.flush()
 }
@@ -14,4 +14,29 @@ fn write_bytes() {
     out.write(b"Hello, World\n").unwrap();
 
     assert_eq!(out, bytes);
+}
+
+pub fn top_ten<T: Debug + Hash + Ord + Clone>(values: &mut Vec<T>) -> Vec<T> {
+    let mut vec = Vec::with_capacity(10);
+
+    values.sort();
+
+    for i in 0..10 {
+        match values.get(i) {
+            Some(value) => vec.push(value.clone()),
+            None => break,
+        }
+    }
+
+    vec
+}
+
+#[test]
+fn top_ten_test() {
+    let mut values = (1..100).collect::<Vec<u32>>();
+
+    let actual = top_ten(&mut values);
+
+    assert_eq!(99, values.len());
+    assert_eq!(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10], actual);
 }
