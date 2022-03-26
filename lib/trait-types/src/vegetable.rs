765587c8a5ccc8a574a9c11108b8408c864366b9
ishibashi-futos <ishibashi.futos@outlook.com>
2022-03-26 21:32:55 +0900

    トレイトとジェネリクスどちらを使うべきか オーダーの中にはVegetable型の他のサラダも入れられるようにしたいので、トレイトを使用する。 しかし、トレイトはメモリ上のサイズが決まっていないため`Vec<dyn Vegitable>`は使用できない Boxでラップすることで、ヒープ上にメモリを確保し値を格納できる

---
+++ lib/trait-types/src/vegetable.rs
@@ -0,0 +1,36 @@
+pub trait Vegetable {
+    fn name(&self) -> &'static str;
+}
+
+pub struct Salad {
+    pub veggies: Vec<Box<dyn Vegetable>>,
+}
+
+impl Salad {
+    pub fn new(veggies: Vec<Box<dyn Vegetable>>) -> Salad {
+        Salad { veggies }
+    }
+}
+
+pub struct Lettuce {
+}
+
+impl Vegetable for Lettuce {
+    fn name(&self) -> &'static str {
+        "Lettuce"
+    }
+}
+
+#[test]
+fn order_salad() {
+
+    let salad = Salad::new(
+        vec![
+            Box::new(Lettuce {})
+        ]
+    );
+
+    let lettuce = salad.veggies.get(0).unwrap();
+
+    assert_eq!("Lettuce", lettuce.name());
+}
